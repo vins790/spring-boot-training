631701a89c5d42b9fefb0dfea994b62aae78266c
Vins <insene790@gmail.com>
2026-02-09 10:12:32 +0100

    Initial commit

---
+++ src/main/java/com/vins/spring_boot_training/SpringBootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.vins.spring_boot_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vins/spring_boot_training/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vins.spring_boot_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
