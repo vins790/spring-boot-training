ffe28a0583b7a2ed70d0e0e06756acce35272694
Vins <insene790@gmail.com>
2026-02-16 16:36:00 +0100

    Sentences history

--- src/main/java/com/vins/spring_boot_training/service/WordsService.java
+++ src/main/java/com/vins/spring_boot_training/service/WordsService.java
@@ -1,9 +1,11 @@
 package com.vins.spring_boot_training.service;
 
+import com.vins.spring_boot_training.entity.Sentence;
 import com.vins.spring_boot_training.entity.User;
 import com.vins.spring_boot_training.entity.Word;
 import com.vins.spring_boot_training.exception.CustomException;
 import com.vins.spring_boot_training.exception.errors.UserErrors;
+import com.vins.spring_boot_training.repository.SentencesRepository;
 import com.vins.spring_boot_training.repository.UsersRepository;
 import com.vins.spring_boot_training.repository.WordsRepository;
 import jakarta.transaction.Transactional;
@@ -21,22 +23,25 @@ import static java.util.Collections.emptySet;
 public class WordsService {
   private final WordsRepository wordsRepository;
   private final UsersRepository usersRepository;
+  private final SentencesRepository sentencesRepository;
 
   private Set<String> extractWords(String sentence) {
     if (sentence == null || sentence.isEmpty()) {
       return emptySet();
     }
 
-    return Arrays.stream(sentence.split("[^a-zA-Z]+"))
+    return Arrays.stream(sentence.split("[^a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+"))
         .filter(word -> !word.isEmpty())
         .map(String::toLowerCase)
         .collect(Collectors.toSet());
   }
 
   public WordsService(WordsRepository wordsRepository,
-                      UsersRepository usersRepository) {
+                      UsersRepository usersRepository,
+                      SentencesRepository sentencesRepository) {
     this.wordsRepository = wordsRepository;
     this.usersRepository = usersRepository;
+    this.sentencesRepository = sentencesRepository;
   }
 
   @Transactional
@@ -44,10 +49,13 @@ public class WordsService {
     User user = usersRepository.findById(userId)
         .orElseThrow(() -> new CustomException(UserErrors.USER_INVALID_USERNAME));
 
+    Sentence sentenceEntity = new Sentence(sentence, user);
+    sentencesRepository.save(sentenceEntity);
+
     Set<String> wordsToSave = this.extractWords(sentence);
     wordsToSave.forEach(word -> {
       Word wordEntity = wordsRepository.findByWord(word)
-          .orElseGet(() -> wordsRepository.save(new Word(word)));
+          .orElseGet(() -> wordsRepository.save(new Word(word, sentenceEntity)));
       user.getWords().add(wordEntity);
     });
     usersRepository.save(user);
